b979f14114dc33a980332b4fd40b414f6b8a7836
Era206 <1805116@ugrad.cse.buet.ac.bd>
2023-08-06 20:25:33 +0600

    dashboeard

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import './index.css';
+import logo from './header_image.png';
 
 const Header = () => {
     return (
-        <header className="bg-bottle-green">
-          <div className="container text-center">
-            <h1 className="text-light">Academic Analysis</h1>
-            <p className="lead text-light">Check out your Safe options, probable chances and too ambitious list!</p>
-          </div>
-        </header>
+      <header className="bg-bottle-green py-1">
+      <div className="container text-center">
+        <img src={logo} alt="Logo" className="logo-img" />
+        <h1 className="text-light display-4">Academic Analysis</h1>
+        <p className="lead text-light">Check out your Safe options, probable chances and too ambitious list!</p>
+      </div>
+    </header>
         
       );
 }
