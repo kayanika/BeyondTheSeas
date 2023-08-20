038de9c0db8407d6b6637ae1addf282b3e02a8fc
Era206 <1805116@ugrad.cse.buet.ac.bd>
2023-08-21 02:20:08 +0600

    forum frontend updated: showing questions and ask a question form done

--- client/src/components/forum.jsx
+++ client/src/components/forum.jsx
@@ -1,148 +1,237 @@
-import React, { useState } from 'react';
-import { Paper, Typography, Button, TextField, Box } from '@mui/material';
+// Forum.js
+import {  Pagination, PaginationItem } from '@mui/material';
+import './index.css';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+
+
+
+
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import {
+  Container,
+  Box,
+  TextField,
+  Button,
+  CircularProgress,
+  Snackbar,
+  IconButton,
+} from '@mui/material';
+import { FaSearch } from 'react-icons/fa';
+import beyondTheSeas from '../apis/beyondTheSeas';
+import AskQuestionForm from './AskQuestionForm'; // Update the path as needed
+import CloseIcon from '@mui/icons-material/Close';
 
 const Forum = () => {
-  const [forumData, setForumData] = useState([
-    {
-      question: 'How does React work?',
-      answers: ['React is a JavaScript library for building user interfaces.', 'It uses a virtual DOM to optimize updates.'],
-      isEditable: false,
-      
-    },
-    {
-      question: 'What is the purpose of JSX?',
-      answers: ['JSX allows you to write HTML-like syntax in your JavaScript code.', 'It\'s used to describe the structure of UI components.'],
-      isEditable: false,
-    }
-    // Add more questions and answers as needed
-  ]);
+  const { userID } = useParams();
+  const [forumData, setForumData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [page, setPage] = useState(1); // Start from page 1
+  const rowsPerPage = 5; // Adjust as needed
+  const [isAskQuestionOpen, setIsAskQuestionOpen] = useState(false);
+  const [isSuccessSnackbarOpen, setIsSuccessSnackbarOpen] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  // const [isSuccessSnackbarOpen, setIsSuccessSnackbarOpen] = useState(false);
 
-  
-
-  const [newQuestion, setNewQuestion] = useState('');
-  const [newAnswer, setNewAnswer] = useState('');
-
-  const handleAddAnswer = (index) => {
-    const updatedForumData = [...forumData];
-    updatedForumData[index].answers.push(newAnswer);
-    setForumData(updatedForumData);
-    setNewAnswer('');
-  };
-
-  const handleAddQuestion = () => {
-    const newQuestionObj = {
-      question: newQuestion,
-      answers: [],
-      isEditable: true,
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await beyondTheSeas.get(`/view-forum/${userID}`);
+        setForumData(response.data.data.user);
+      } catch (err) {
+        console.log(err);
+      }
     };
-    setForumData([...forumData, newQuestionObj]);
-    setNewQuestion('');
+    fetchData();
+  }, [userID]);
+
+  const handleSearch = async () => {
+    try {
+      const response = await beyondTheSeas.post(`/view-forum/${userID}/searchTopic`, {
+        searchTopic: searchQuery,
+      });
+      setForumData(response.data.data.user);
+      setSearchQuery('');
+      setPage(1);
+    } catch (error) {
+      console.error('Error performing search:', error);
+    }
   };
 
-  const handleEditAnswer = (questionIndex, answerIndex) => {
-    const updatedForumData = [...forumData];
-    updatedForumData[questionIndex].isEditable = true;
-    setNewAnswer(updatedForumData[questionIndex].answers[answerIndex]);
-    setForumData(updatedForumData);
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
   };
 
-  const handleSaveEditAnswer = (questionIndex, answerIndex) => {
-    const updatedForumData = [...forumData];
-    updatedForumData[questionIndex].answers[answerIndex] = newAnswer;
-    updatedForumData[questionIndex].isEditable = false;
-    setForumData(updatedForumData);
-    setNewAnswer('');
+  const handleAskQuestion = async (questionText, keywords) => {
+    if (!questionText.trim()) {
+      return; // Don't insert empty questions
+    }
+
+    try {
+      setIsSubmitting(true); // Start the loading state
+      const response = await beyondTheSeas.post(`/view-forum/${userID}/postQuestion`, {
+        question_text: questionText,
+        keywords: keywords,
+      });
+      setIsSuccessSnackbarOpen(true);
+      setIsAskQuestionOpen(false);
+    } catch (error) {
+      console.error('Error posting question:', error);
+    } finally {
+      setIsSubmitting(false); // End the loading state, regardless of success or error
+    }
   };
 
-  const handleDeleteAnswer = (questionIndex, answerIndex) => {
-    const updatedForumData = [...forumData];
-    updatedForumData[questionIndex].answers.splice(answerIndex, 1);
-    setForumData(updatedForumData);
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const renderQuestions = () => {
+    const startIndex = (page - 1) * rowsPerPage;
+    const endIndex = startIndex + rowsPerPage;
+
+    return forumData
+      .slice(startIndex, endIndex)
+      .map((questionObj, index) => (
+        <Box
+          key={index}
+          sx={{
+            marginBottom: '20px',
+            padding: '10px',
+            backgroundColor: 'rgba(255, 255, 255, 0.5)',
+            boxShadow: 1,
+            width: '100%',
+          }}
+        >
+          <Button
+            variant="text"
+            color="success"
+            style={{
+              marginBottom: '10px',
+              width: '100%',
+              textAlign: 'left',
+              border: 'none',
+              borderRadius: '0',
+              paddingLeft: '0',
+            }}
+            onClick={() =>
+              (window.location.href = `/api/user/view-forum/${userID}/getQuestion/${questionObj.question_id}`)
+            }
+          >
+            {startIndex + index + 1}. {questionObj.question_text}
+          </Button>
+        </Box>
+      ));
   };
 
   return (
-    <div>
-      <h1>Forum</h1>
-      <div style={{ display: 'flex', alignItems: 'center', marginRight: '10px', marginBottom: '10px' }}>
-      <TextField
-        label="Ask a question"
-        value={newQuestion}
-        onChange={(e) => setNewQuestion(e.target.value)}
-        variant="outlined"
-        margin="dense"
+    <Container
+    sx={{
+      backgroundImage: 'url("../images/background4.jpg")',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      minHeight: '94vh',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    }}
+  >
+      <Box
+        style={{
+          position: 'sticky',
+          top: 45,
+          left: 260,
+          width: '100%',
+          padding: '15px',
+          marginTop: '30px',
+          display: 'flex',
+        }}
+      >
+        <TextField
+          label="Search for topics"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          onKeyPress={handleKeyPress}
+          variant="outlined"
+          margin="dense"
+          fullWidth
+          InputProps={{
+            endAdornment: (
+              <Button onClick={handleSearch} color="success">
+                <FaSearch />
+              </Button>
+            ),
+          }}
+        />
+      </Box>
+      <Box
+        style={{
+          marginTop: '20px',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        {renderQuestions()}
+        <Pagination
+          count={Math.ceil(forumData.length / rowsPerPage)}
+          page={page}
+          onChange={handleChangePage}
+          renderItem={(item) => (
+            <PaginationItem
+              slots={{ previous: ArrowBackIcon, next: ArrowForwardIcon }}
+              {...item}
+            />
+          )}
+          />
+        <Button
+          variant="outlined"
+          color="primary"
+          style={{ marginTop: '20px' }}
+          onClick={() => setIsAskQuestionOpen(true)}
+        >
+          Ask A Question
+        </Button>
+        {/* {isAskQuestionOpen && (
+          <AskQuestionForm
+            onClose={() => setIsAskQuestionOpen(false)}
+            onSubmit={handleAskQuestion}
+          />
+        )} */}
+        {isAskQuestionOpen && (
+        <AskQuestionForm
+          userID={userID}
+          onClose={() => setIsAskQuestionOpen(false)}
+          onSubmit={handleAskQuestion}
+          isSubmitting={isSubmitting} // Pass the loading state to the form
+        />
+      )}
+
+      <Snackbar
+        open={isSuccessSnackbarOpen}
+        autoHideDuration={5000}
+        onClose={() => setIsSuccessSnackbarOpen(false)}
+        message="Question added to the server successfully"
+        action={
+          <IconButton
+            size="small"
+            aria-label="close"
+            color="inherit"
+            onClick={() => setIsSuccessSnackbarOpen(false)}
+          >
+            <CloseIcon fontSize="small" />
+          </IconButton>
+        }
       />
-      <Button variant="outlined" color="success" onClick={handleAddQuestion}  style={{ marginLeft: '10px' }}>
-        Add Question
-      </Button>
-      </div>
-      <Paper elevation={3} style={{ padding: '20px', marginTop: '20px' }}>
-        {forumData.map((questionObj, questionIndex) => (
-          <div key={questionIndex} style={{ marginBottom: '20px' }}>
-            <Typography variant="h6">{questionObj.question}</Typography>
-            {questionObj.answers.map((answer, ansIndex) => (
-              <div key={ansIndex} style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
-                {questionObj.isEditable && ansIndex === questionObj.answers.length - 1 ? (
-                  <Box sx={{ display: 'flex', alignItems: 'center', marginRight: '10px' }}>
-                    <TextField
-                      label="Edit answer"
-                      value={newAnswer}
-                      onChange={(e) => setNewAnswer(e.target.value)}
-                      variant="outlined"
-                      margin="dense"
-                    />
-                    <Button
-                      variant="outlined"
-                      color="success"
-                      size="small"
-                      onClick={() => handleSaveEditAnswer(questionIndex, ansIndex)}
-                    >
-                      Save
-                    </Button>
-                  </Box>
-                ) : (
-                  <Typography variant="body1" style={{ marginRight: '10px' }}>
-                    {answer}
-                  </Typography>
-                )}
-                {!questionObj.isEditable && ansIndex === questionObj.answers.length - 1 && (
-                  <Button
-                    variant="outlined"
-                    color="info"
-                    size="small"
-                    onClick={() => handleEditAnswer(questionIndex, ansIndex)}
-                  >
-                    Edit
-                  </Button>
-                )}
-                <Button
-                  variant="outlined"
-                  color="error"
-                  size="small"
-                  onClick={() => handleDeleteAnswer(questionIndex, ansIndex)}
-                >
-                  Delete
-                </Button>
-              </div>
-            ))}
-            {!questionObj.isEditable && (
-              <div style={{ display: 'flex', alignItems: 'center', marginRight: '10px', marginBottom: '10px' }}>
-                <TextField
-                  label="Add an answer"
-                  value={newAnswer}
-                  onChange={(e) => setNewAnswer(e.target.value)}
-                  variant="outlined"
-                  margin="dense"
-                />
-                <Button variant="outlined" color="success" onClick={() => handleAddAnswer(questionIndex)} style={{ marginLeft: '10px' }}>
-                  Add Answer
-                </Button>
-              </div>
-            )}
-          </div>
-        ))}
-      </Paper>
-    </div>
+        
+      </Box>
+    </Container>
   );
 };
 
 export default Forum;
+
