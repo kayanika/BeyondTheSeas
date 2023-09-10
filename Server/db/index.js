26ed28219ee012aa6b79d82757e183cb35a927d9
kayanika <1805110@ugrad.cse.buet.ac.bd>
2023-09-10 16:40:04 +0600

    201

--- Server/db/index.js
+++ Server/db/index.js
@@ -4,15 +4,15 @@ const { Pool } = require('pg');
 const pool = new Pool(
     {
 
-   host: 'dpg-cjd0jp2nip6c739lreng-a.singapore-postgres.render.com',
-  database: 'beyondtheseas',
-  password: 'KOzPun0L1NwkLqedJABdhktUvN38C630',
-//   user:'postgres',
-//   host:'localhost',
-//     database:'beyondtheseasvs2',
-//     password:'era123',
-//   port: 5432,
-ssl : true,
+//    host: 'dpg-cjd0jp2nip6c739lreng-a.singapore-postgres.render.com',
+//   database: 'beyondtheseas',
+//   password: 'KOzPun0L1NwkLqedJABdhktUvN38C630',
+  user:'postgres',
+  host:'localhost',
+    database:'BeyondTheSeas',
+    password:'1234',
+  port: 5432,
+// ssl : true,
 
     }
 )
